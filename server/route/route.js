d7564b5db89436fe8c02c956fbc6a6cddc352575
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-08-11 22:20:10 +0530

    Added the markAttendance route

--- server/route/route.js
+++ server/route/route.js
@@ -25,7 +25,32 @@ router.post('/addStudent', async (req, res) => {
     } catch (error) {
       res.status(500).json({ error: 'Error adding student' });
     }
-  });
+});
+
+
+router.post('/markAttendance', async (req, res) => {
+    const { name } = req.body;
+    const currentDate = new Date().toLocaleDateString('en-US', {
+      day: 'numeric',
+      month: 'numeric',
+      year: 'numeric',
+    });
   
+    try {
+      const student = await Student.findOne({ name });
+  
+      if (!student) {
+        return res.status(404).json({ error: 'Student not found' });
+      }
+  
+      student.attendance.set(currentDate, 'P');
+      await student.save();
+  
+      res.json({ message: 'Attendance marked successfully' });
+    } catch (error) {
+      res.status(500).json({ error: 'Error marking attendance' });
+    }
+});
+    
 
 module.exports = router
