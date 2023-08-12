9c7f3c8aee981ea1b756d467cb7bef249c4e705a
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-08-12 22:31:07 +0530

    basic registration added

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Bugfender } from '@bugfender/sdk';
 
 export const Register = () => {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+        const response = await axios.post('http://localhost:8000/addStudent', {
+            name,
+            password,
+          });
+          
+
+      console.log(response.data);
+      Bugfender.log(response.data);
+      
+    
+    } catch (error) {
+      console.error(error);
+      Bugfender.log(error);
+      
+    }
+  };
+
   return (
     <div className='flex justify-center mt-20'>
       <div className='flex flex-col h-[500px] w-[500px] bg-blue-900 border-2 border-black rounded-lg p-4'>
         <div className='flex flex-col mt-2 self-center'>
           <label className='text-white text-lg self-start ml-2'>Name</label>
-          <input className='w-[300px] h-[40px] border-2 border-black rounded-lg p-2 mt-1' placeholder='Enter your name...' />
+          <input
+            className='w-[300px] h-[40px] border-2 border-black rounded-lg p-2 mt-1'
+            placeholder='Enter your name...'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
         <div className='flex flex-col mt-6 self-center'>
           <label className='text-white text-lg self-start ml-2'>Password</label>
-          <input className='w-[300px] h-[40px] border-2 border-black rounded-lg p-2 mt-1' placeholder='Enter a strong password...' />
+          <input
+            className='w-[300px] h-[40px] border-2 border-black rounded-lg p-2 mt-1'
+            placeholder='Enter a strong password...'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
 
         <div className='flex flex-col mt-4 w-[300px] self-center'>
@@ -23,9 +59,11 @@ export const Register = () => {
           <label className='text-white text-lg self-start ml-2'>Upload Image 2</label>
           <input type='file' accept='image/*' className='border-2 border-white rounded-lg p-2 mt-1' />
         </div>
-        
-        {/* Button */}
-        <button className='bg-white border-2 border-black text-black w-[300px] h-[40px] rounded-lg self-center mt-10'>
+
+        <button
+          className='bg-white border-2 border-black text-black w-[300px] h-[40px] rounded-lg self-center mt-10'
+          onClick={handleSubmit}
+        >
           Register
         </button>
       </div>
