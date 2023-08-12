7beca0059296f22ad21a163544823a270a22685e
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-08-12 20:11:42 +0530

    Set up bugfender sdk

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { Bugfender } from '@bugfender/sdk';
+
+Bugfender.init({
+    appKey: 'sOHsa19oFfWFjpZF7TjkcRv72T0yACcd',
+    // apiURL: 'https://api.bugfender.com',
+    // baseURL: 'https://dashboard.bugfender.com',
+    // overrideConsoleMethods: true,
+    // printToConsole: true,
+    // registerErrorHandler: true,
+    // logBrowserEvents: true,
+    // logUIEvents: true,
+    // version: '',
+    // build: '',
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
