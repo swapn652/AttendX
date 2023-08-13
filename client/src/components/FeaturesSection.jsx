86088c81a0fb259ebc54fde19d039c7249dcf5a7
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-08-13 23:03:18 +0530

    Fixed some design issues

--- client/src/components/FeaturesSection.jsx
+++ client/src/components/FeaturesSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const FeaturesSection = () => {
   return (
-    <div className='bg-blue-900 h-72 w-screen -ml-8 flex justify-center items-center mt-10'>
+    <div className='bg-blue-900 h-72 w-screen -ml-[32px] xl:-ml-[32px] flex justify-center items-center mt-10'>
       <div className='flex gap-8 max-w-screen-lg mx-auto'>
         <div className='bg-white h-60 w-80 p-4 rounded-lg'>
           <h3 className='text-xl font-semibold mb-2'>Use AI Recognition to Mark Attendance</h3>
