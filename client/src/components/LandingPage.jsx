f5801f35d574cce654aeff909ec1972ec6c3d692
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-08-13 19:41:03 +0530

    Added the LandingPage component

---
+++ client/src/components/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { MainSection } from './MainSection'
+import { FeaturesSection } from './FeaturesSection'
+import { TechStackSection } from './TechStackSection'
+import { Footer } from './Footer'
+
+export const LandingPage = () => {
+  return (
+  <>
+      <MainSection/>
+      <FeaturesSection/>
+      <TechStackSection/>
+      <Footer/>
+  </>
+  )
+}
