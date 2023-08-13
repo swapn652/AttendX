ffd2f4760b3d9beef0c91908b851125d47e9db24
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-08-13 18:02:58 +0530

    Added the login functionality

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Login = () => {
+  const [rollId, setRollId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:8000/login', {
+        rollId,
+        password
+      });
+
+      const { message, studentName, studentRollId } = response.data;
+      toast.success(message);
+
+      // You can use studentName and studentRollId as needed, e.g. store them in state or context
+
+    } catch (error) {
+      console.error(error);
+      toast.error('Login failed. Please check your credentials.');
+    }
+  };
+
   return (
     <div className='flex justify-center mt-20'>
       <div className='flex flex-col h-[300px] w-[400px] bg-blue-900 border-2 border-black rounded-lg p-4'>
@@ -9,6 +33,8 @@ export const Login = () => {
           <input
             className='w-[300px] h-[40px] border-2 border-black rounded-lg p-2 mt-1'
             placeholder='Enter your Roll ID...'
+            value={rollId}
+            onChange={(e) => setRollId(e.target.value)}
           />
         </div>
 
@@ -18,10 +44,15 @@ export const Login = () => {
             type='password'
             className='w-[300px] h-[40px] border-2 border-black rounded-lg p-2 mt-1'
             placeholder='Enter your password...'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
-        <button className='bg-white border-2 border-black text-black w-[300px] h-[40px] rounded-lg self-center mt-6'>
+        <button
+          className='bg-white border-2 border-black text-black w-[300px] h-[40px] rounded-lg self-center mt-6'
+          onClick={handleLogin}
+        >
           Login
         </button>
       </div>
