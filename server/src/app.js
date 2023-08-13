1aa246c349a385a6b317da9502101151ef59acbf
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-08-14 00:38:55 +0530

    Fixed an issue related to dotenv

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
-require('dotenv').config({ path: '../.env' });
+const dotenv = require('dotenv');
+dotenv.config({ path: '../.env' });
 const express = require('express');
 const app = express();
 require('../db/conn');
