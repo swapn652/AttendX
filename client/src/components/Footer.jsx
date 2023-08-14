e05abbd776c3435d8884b7298843418532e92b90
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-08-14 19:41:52 +0530

    Made the techstack icons center

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer = () => {
   return (
-    <div className='bg-blue-900 -ml-[32px] text-white text-lg h-20 w-screen relative -mb-8 bottom-0 left-0 flex justify-center items-center'>
+    <div className='bg-blue-900 -ml-[32px] text-white text-lg h-20 w-screen relative -mb-8 bottom-0 left-0 flex justify-center items-center md:mt-10 lg:mt-0'>
       Made with ❤ by Swapnil Pant
     </div>
   );
--- client/src/components/TechStackSection.jsx
+++ client/src/components/TechStackSection.jsx
@@ -34,16 +34,18 @@ export const TechStackSection = () => {
         </div>
       </div>
     </div>
-    <div className='flex flex-col sm:block md:hidden mt-10 gap-y-10'>
-        <img src="/techStack/react.png" alt="" className='ml-6 mt-8 h-[80px] w-[250px]' />
-        <img src="/techStack/tailwind.png" alt="" className='ml-6 h-[140px] w-[250px]' />
-        <img src="/techStack/node.png" alt="" className='ml-10 h-[140px] w-[250px]' />
-        <img src="/techStack/express.png" alt="" className='ml-8 h-[140px] w-[250px]' />
-        <img src="/techStack/mongodb.png" alt="" className='ml-14 h-[120px] w-[200px]' />
-        <div className='h-20 w-60 ml-10 bg-blue-900 border-2 border-black rounded-lg flex justify-center items-center mt-10'>
-          <span className='text-white text-2xl'>Face-api.js</span>
-        </div>
+    <div className='flex flex-col sm:flex sm:items-center md:hidden mt-10 gap-y-10 mx-auto justify-center w-full max-w-screen-lg'>
+    <img src="/techStack/react.png" alt="" className='mx-auto mt-8 h-[80px] w-[250px]' />
+    <img src="/techStack/tailwind.png" alt="" className='mx-auto h-[140px] w-[250px]' />
+    <img src="/techStack/node.png" alt="" className='mx-auto h-[140px] w-[250px]' />
+    <img src="/techStack/express.png" alt="" className='mx-auto h-[140px] w-[250px]' />
+    <img src="/techStack/mongodb.png" alt="" className='mx-auto h-[120px] w-[200px]' />
+    <div className='h-20 w-60 mx-auto bg-blue-900 border-2 border-black rounded-lg flex justify-center items-center mt-10 mb-10'>
+        <span className='text-white text-2xl'>Face-api.js</span>
     </div>
+</div>
+
+
 
 
     </>
