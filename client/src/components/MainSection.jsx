399cad8a8a8d1b00da519ea5e35ed1f4c2cecceb
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-08-14 19:23:59 +0530

    Made the main section work for mobile screen devices

--- client/src/components/MainSection.jsx
+++ client/src/components/MainSection.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 export const MainSection = () => {
   return (
     <div className='flex flex-col mt-24'>
-      <div style={{ fontFamily: 'Dancing Script' }} className='text-9xl text-blue-900'>
+      <div style={{ fontFamily: 'Dancing Script' }} className='text-7xl md:text-9xl text-blue-900'>
         AttendX
       </div>
-      <div className='w-[400px] self-center text-2xl'>
+      <div className='md:w-[400px] self-center md:text-2xl'>
         Streamline attendance with ease using AttendX. 
         Say hello to a smarter way of tracking presence, where faces become signatures. 
         Simplify attendance management, embrace AttendX.
